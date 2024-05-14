27adbd0ee13ec7b165d165d752b55bd48058a7f5
Will Rossiter <will.rossiter@akqa.com>
2024-05-14 14:52:43 +1200

    Update Registry.tsx

--- app/client/src/state/Registry.tsx
+++ app/client/src/state/Registry.tsx
@@ -1,53 +1,87 @@
-import React from "react";
+import { Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
-import Banner from "@/components/Banner";
+
+/**
+ * Registry is a component registry that allows you to register components and apply them to the DOM.
+ *
+ * As you create new components in your frontend, you can register them here and they will be automatically
+ * applied to the DOM when the page loads. To indicate in the liquid template what component should be
+ * rendered, add a `data-component` attribute to the element with the name of the component.
+ *
+ * Props can be passed to the component by adding a `data-props` attribute to the element with a JSON object or you
+ * can use the `__PROPS` helper to pass dynamic props to the component. For instance,
+ *
+ * ```html
+ * <script>
+ * if (typeof __PROPS === 'undefined') __PROPS = {};
+ * __PROPS[somekey] = {
+ *    field: "value",
+ *    field2: "value2"
+ * };
+ * </script>
+ * <div data-component="FeaturedLinks" data-props="__PROPS[somekey]"></div>
+ * ```
+ */
+const components = {
+  Banner: lazy(() => import("@/Components/Banner"))
+};
 
 const Registry = {
-    components: {
-        Banner: Banner,
-    },
-    register: (key, component) => {
-        Registry.components[key] = component;
-    },
-    merge: (components) => {
-        Registry.components = { ...Registry.components, ...components };
-    },
-    applyTo: (document) => {
-        const body = document.querySelector("body");
+  components: components,
+  register: (key, component) => {
+    Registry.components[key] = component;
+  },
+  merge: (components) => {
+    Registry.components = { ...Registry.components, ...components };
+  },
+  applyTo: (document: Document) => {
+    const body = document.querySelector("body");
 
-        if (body) {
-            Object.keys(Registry.components).map((name) => {
-                const el = document.querySelectorAll(
-                    `[data-component="${name}"]`
-                );
+    if (body) {
+      Object.keys(Registry.components).map((name) => {
+        const el = document.querySelectorAll(`[data-component="${name}"]`);
 
-                el.forEach((element) => {
-                    const Component = Registry.components[name];
+        el.forEach((element) => {
+          const Component = Registry.components[name];
 
-                    if (element) {
-                        try {
-                            const props = element.hasAttribute("data-props")
-                                ? JSON.parse(
-                                      element.getAttribute("data-props") || "{}"
-                                  )
-                                : {};
+          if (element) {
+            try {
+              let componentProps = {};
+              const staticProps = element.hasAttribute("data-props")
+                ? element.getAttribute("data-props")
+                : "";
 
-                            const component = <Component {...props} />;
-                            const root = createRoot(element);
-                            root.render(component);
-                        } catch (e) {
-                            console.error(`Error rendering component ${name}`);
-                            console.error(e);
-                        }
-                    }
-                });
-            });
+              if (staticProps.substring(0, 1) === "{") {
+                componentProps = JSON.parse(staticProps);
+              } else if (staticProps.substring(0, 7) === "__PROPS") {
+                componentProps = eval(staticProps);
+              }
 
-            setTimeout(() => {
-                body.classList.add("energized");
-            }, 50);
-        }
-    },
+              // replace with loading spinner?
+              const fallback = null;
+
+              const component = (
+                <Suspense fallback={fallback}>
+                  <Component {...componentProps} />
+                </Suspense>
+              );
+
+              const root = createRoot(element);
+
+              root.render(component);
+            } catch (e) {
+              console.error(`Error rendering component ${name}`);
+              console.error(e);
+            }
+          }
+        });
+      });
+
+      setTimeout(() => {
+        body.classList.add("lights--on");
+      }, 50);
+    }
+  },
 };
 
 export default Registry;
